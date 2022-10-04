1af3e92f3d31e16c79be2313cc69f88d93dcf41b
Christian Liebel <christian.liebel@thinktecture.com>
2022-10-04 21:50:51 +0200

    Lab #5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,7 @@ export class AppComponent implements AfterViewInit {
   // LAB #2.1
   context?: CanvasRenderingContext2D;
   // LAB #5
+  previousPoint?: { x: number, y: number };
   // LAB #11
   // LAB #17
 
@@ -35,15 +36,23 @@ export class AppComponent implements AfterViewInit {
 
   onPointerDown(event: PointerEvent) {
     // LAB #5
+    this.previousPoint = { x: event.offsetX, y: event.offsetY };
   }
 
   onPointerMove(event: PointerEvent) {
     // LAB #4 and 5
-    this.context!.fillRect(event.offsetX, event.offsetY, 2, 2);
+    if (this.previousPoint) {
+      const currentPoint = { x: event.offsetX, y: event.offsetY };
+      for (const point of this.paintService.bresenhamLine(this.previousPoint, currentPoint)) {
+        this.context!.fillRect(point.x, point.y, 2, 2);
+      }
+      this.previousPoint = currentPoint;
+    }
   }
 
   onPointerUp() {
     // LAB #5
+    this.previousPoint = undefined;
   }
 
   onColorChange(color: HTMLInputElement) {
